1575c958d8520ec1e792a9c0d848861959bf5dea
JAIROACARDOZO15 <jairo2224637@correo.uis.edu.co>
2022-11-19 11:35:49 -0500

    SUBIENDO FECHA DETALLADA

--- modelo/FechaDetallada.java
+++ modelo/FechaDetallada.java
@@ -2,8 +2,23 @@ package modelo;
 
 public class FechaDetallada extends Fecha
 {
-    public FechaDetallada (int dia, int mes, int anio)
+    String[] Mes = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"};
+    
+
+    public FechaDetallada(int dia, int mes, int anio) 
     {
-        String[] Mes = { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviemnre", "Diciembre"};
+        super(dia, mes, anio);
     }
+
+    public String mesToString() 
+    {
+        return Mes[this.mes - 1];
+    }
+
+    @Override
+    public String toString() 
+    {
+        return dia + " de " + mesToString() + " de " + anio;
+    }
+
 }
